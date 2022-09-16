56e1bfcbb254e35c6a1945bec4d5b166cb446903
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-09-16 02:15:20 -0300

    :feat: listing ad by game

--- esports/server/src/server.ts
+++ esports/server/src/server.ts
@@ -22,18 +22,37 @@ app.post("/ads", (request, response) => {
   return response.status(201).json([]);
 });
 
-app.get("/games/:id/ads", (request, response) => {
-  // const gameId = request.params.id;
+app.get("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
 
-  return response.json([
-    { id: 1, name: "Anúncio 1" },
-    { id: 2, name: "Anúncio 2" },
-    { id: 3, name: "Anúncio 3" },
-  ]);
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourtEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(','),
+    };
+  }));
 });
 
 app.get("/ads/:id/discord", (request, response) => {
-  // const adId = request.params.id;
+  const adId = request.params.id;
 
   return response.json([
     { id: 1, name: "Anúncio 1" },
